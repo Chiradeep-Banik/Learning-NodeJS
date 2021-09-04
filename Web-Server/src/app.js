20d60b5c0966ff5b8fcfab4872f318772473a0a8
Chiradeep-Banik <chiradeep13@gmail.com>
2021-09-04 23:34:17 +0530

    modified_web_Server

--- Web-Server/src/app.js
+++ Web-Server/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-console.log(__dirname);
+
 const app = express(); // creating an express app
+app.set('view engine','hbs');
 
 //Express is a web framework for Node.js
 app.use(express.static("/mnt/c/Users/chira/OneDrive/Desktop/CODE/NodeJs/Web-Server/public"));//Serves up a static file for index.html ar /
 app.use(express.static("/mnt/c/Users/chira/OneDrive/Desktop/CODE/NodeJs/Web-Server/public/"));
-app.get('/weather',(req,res)=>{
+app.get('/weather', (req, res) => {
     res.send("we");
 });//Set up the / root page and what the user would get when they visit the root page
 
-app.listen(1313,()=>console.log("Started at port 1313"));// started up the server at port 1313
+app.listen(1313, () => console.log("Started at port 1313"));// started up the server at port 1313
