29b7568ee2fb918f700a364ebc63e60e36d2fc1f
Chiradeep-Banik <chiradeep13@gmail.com>
2021-09-05 02:33:42 +0530

    mod_express_app

--- Web-Server/src/app.js
+++ Web-Server/src/app.js
@@ -5,7 +5,7 @@ app.set('view engine','hbs');
 
 //Express is a web framework for Node.js
 app.use(express.static("/mnt/c/Users/chira/OneDrive/Desktop/CODE/NodeJs/Web-Server/public"));//Serves up a static file for index.html ar /
-app.use(express.static("/mnt/c/Users/chira/OneDrive/Desktop/CODE/NodeJs/Web-Server/public/"));
+//app.use(express.static("/mnt/c/Users/chira/OneDrive/Desktop/CODE/NodeJs/Web-Server/public/"));
 app.get('/weather', (req, res) => {
     res.send("we");
 });//Set up the / root page and what the user would get when they visit the root page
